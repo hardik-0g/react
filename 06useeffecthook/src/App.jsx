8eee65c9b6560e99af6f6cc1b51632e577b39c7f
hardik-0g <hardikjaiswal88888@gmail.com>
2026-02-04 00:17:07 +0530

    useEffect hook all variations

---
+++ 06useeffecthook/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [total,setTotal]= useState(1);
+  // variation 1 - runs on each render
+  // useEffect(() => {
+  //       alert('i will run at render')
+        
+  //     });
+
+
+// variation 2 - runs on 1st render only
+  // useEffect(() => {
+  //   alert('runs on 1st render only')
+    
+  // }, [])
+  
+  // variation 3 - runs only when count is updated
+  // useEffect(() => {
+  //   alert('run every time when count is updated')
+    
+  // }, [count])
+
+
+
+  // variation 4 - runs only when count or total changes
+  // useEffect(() => {
+  //   alert('run either count or total changes')
+    
+  // }, [count,total])
+  
+
+  // variation 5 - cleanup function used here when count removed from ui then 'count is unmounted from UI' and then count is updated
+  useEffect(() => {
+    alert('count is updated')
+  
+    return () => {
+      alert('count is unmounted from UI')
+    }
+  }, [count]);
+  
+  
+  
+  function handleClick(){
+    setCount(count+1);
+  }
+  function handleClickTotal(){
+    setTotal(total+1);
+  }
+
+  return (
+    <>
+      <h1>hifdffdf</h1>
+      <button onClick={handleClick}>click me</button>
+      <br/>
+      count is: {count};
+      <br/>
+      <button onClick={handleClickTotal}>click me</button>
+      <br/>
+      count is: {total};
+
+      
+    </>
+  )
+}
+
+export default App
